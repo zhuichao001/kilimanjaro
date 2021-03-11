f3edd62f9f5d3350b71ce349d270562660e975c1
zhuichao001 <zhuichao001@163.com>
2021-03-11 17:45:54 +0800

    add rust code

---
+++ rs/geeker.rs
@@ -0,0 +1,208 @@
+
+fn clone(){
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
+}
+
+fn var(){
+    let m = 123;
+    let m = (m+1)/2;
+    println!("m={}", m);
+
+    let mut n: u64 = 234;
+    n -= 10;
+    println!("n={}", n);
+
+    let mut v="yes";
+    println!("v={}, len={}", v, v.len());
+
+    v = "no";
+    println!("v={}, len={}", v, v.len());
+
+}
+
+fn array(){
+    let arr: [u32; 5] = [1, 2, 3, 7, 8];
+    for a in arr.iter(){
+        println!("{}", a);
+    }
+}
+
+fn slice(){
+    let s = String::from("broadcast");
+
+    let part1 = &s[0..5];
+    let part2 = &s[5..9];
+
+    println!("{}={}+{}", s, part1, part2);
+}
+
+fn normal_struct(){
+    struct Site {
+        domain: String,
+        found: u32
+    };
+
+    let runoob = Site {
+        domain: String::from("www.runoob.com"),
+        found: 2013
+    };
+    println!("struct = ({}, {})", runoob.domain, runoob.found);
+}
+
+fn tuple_struct(){
+    struct Point(f64, f64);
+    let pos = Point(0.0, 0.0);
+    println!("pos = ({}, {})", pos.0, pos.1);
+}
+
+fn tuple(){
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    println!("tuple = ({}, {}, {})", tup.0, tup.1, tup.2);
+
+    let (x, y, z) = tup;
+    println!("x={}, y={}, z={}", x, y, z);
+}
+
+fn enumer(){
+    #[derive(Debug)]
+    enum Color {
+            Red, Yellow, Blue
+    };
+    let leaf = Color::Red;
+    println!("color={:?}", leaf);
+}
+
+fn module(){
+    mod nation {
+        pub mod government {
+            pub fn run() {println!("nation::government::run");}
+        }
+
+        pub mod congress {
+            pub fn run() {println!("nation::congress::run");}
+        }
+
+        mod court {
+            fn run() {
+                println!("nation::court::run");
+                super::congress::run();
+            }
+        }
+    }
+    nation::government::run();
+}
+
+fn max(array: &[i32]) -> i32 {
+    let mut max_index = 0;
+    let mut i = 1;
+    while i < array.len() {
+        if array[i] > array[max_index] {
+            max_index = i;
+        }
+        i += 1;
+    }
+    return array[max_index];
+}
+
+fn vector(){
+    let mut v = vec![1, 2, 4, 8];
+    v.push(16);
+    println!("{:?}", v);
+}
+
+fn string(){
+    let s = String::new();
+    let s2 = String::from("hawk");
+    let d = 999.to_string();
+    let f = 1.2.to_string();
+    println!("{} {} {} {}", s, s2, d, f);
+}
+
+fn hashmap(){
+    use std::collections::HashMap;
+
+    let mut map = HashMap::new();
+
+    map.insert("city", "suzhou");
+    map.insert("size", "10");
+
+    println!("{}", map.get("city").unwrap());
+}
+
+fn oop(){
+    pub struct Abs {
+        field: i32,
+    }
+
+    impl Abs {
+        pub fn new(val: i32) -> Abs {
+            Abs {
+                field: val
+            }
+        }
+
+        pub fn method(&self) {
+            println!("calling public Abs::method");
+            self.inner();
+        }
+
+        fn inner(&self) {
+            println!("calling private Abs::inner");
+        }
+    }
+
+    let obj = Abs::new(1024);
+    obj.method();
+}
+
+fn threading(){
+    use std::thread;
+    use std::time::Duration;
+
+    fn function() {
+        for i in 0..5 {
+            println!("spawned thread print {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    }
+
+    let handle = thread::spawn(function);
+    handle.join().unwrap();
+    println!("thread finish.");
+}
+
+fn main(){
+    array();
+    println!("-------------");
+    clone();
+    println!("-------------");
+    slice();
+    println!("-------------");
+    normal_struct();
+    println!("-------------");
+    tuple_struct();
+    println!("-------------");
+    tuple();
+    println!("-------------");
+    enumer();
+    println!("-------------");
+    module();
+    println!("-------------");
+
+    let a = [2, 4, 6, 3, 1];
+    println!("max = {}", max(&a));
+    println!("-------------");
+
+    vector();
+    println!("-------------");
+    string();
+    println!("-------------");
+    hashmap();
+    println!("-------------");
+    oop();
+    println!("-------------");
+    threading();
+    println!("-------------");
+}
