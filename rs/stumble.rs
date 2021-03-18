19650b1572024dd8ad16eefd28540799c1bee205
zhuichao001 <zhuichao001@163.com>
2021-03-17 23:52:39 -0700

    modify

--- rs/stumble.rs
+++ rs/stumble.rs
@@ -351,7 +351,6 @@ fn switch(){
 
 fn closure(){
     fn  closure_function    (i: i32) -> i32 { i + 1 }
-
     let closure_annotated = |i: i32| -> i32 { i + 1 };
     let closure_inferred  = |i     |          i + 1  ;
     let closure_noparam   = || 1;
